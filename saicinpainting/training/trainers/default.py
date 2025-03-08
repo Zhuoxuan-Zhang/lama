ee4a30e7363d30da53f80e50d5b0b467d2c679e3
Zhuoxuan-Zhang <zz3052@columbia.edu>
2025-03-07 22:00:41 -0500

    try confidence >0.7

--- saicinpainting/training/trainers/default.py
+++ saicinpainting/training/trainers/default.py
@@ -171,7 +171,7 @@ class DefaultInpaintingTrainingModule(BaseInpaintingTrainingModule):
             metrics['gen_resnet_pl'] = resnet_pl_value
         symmetry_reward = self.compute_mnist_reward(predicted_img, metadata)
         # now lamda is set to 0.1, working for now
-        scaled_loss = torch.exp(-0.2 *symmetry_reward) * total_loss
+        scaled_loss = torch.exp(-0.1 *symmetry_reward) * total_loss
         # LOGGER.info(f"Symmetry Reward (Mean): {symmetry_reward.item():.4f}")
         # LOGGER.info(f"Total Loss Before Scaling: {total_loss.item():.4f}")
         # LOGGER.info(f"Scaled Loss After Reward: {scaled_loss.item():.4f}")
@@ -194,10 +194,10 @@ class DefaultInpaintingTrainingModule(BaseInpaintingTrainingModule):
             # Compute reward based on MNIST digit presence and equation correctness
             mnist_digit_reward = mnist_digit_count / max(masked_number_length, 1)  # Fraction of valid MNIST digits
             correctness_reward = 1.0 if is_correct else -1.0
-            # confidence_reward = sum(confidences) / max(masked_number_length, 1) if confidences else 0
+            confidence_reward = sum(confidences) / max(masked_number_length, 1) if confidences else 0
             # 0.5 * mnist_digit_reward + 0.3 * correctness_reward + 0.2 * confidence_reward working
-            # total_reward.append(0.3 * mnist_digit_reward + 0.5 * correctness_reward + 0.2 * confidence_reward)
-            total_reward.append(correctness_reward)
+            total_reward.append(0.5 * mnist_digit_reward + 0.3 * correctness_reward + 0.2 * confidence_reward)
+            # total_reward.append(correctness_reward)
 
         total_reward = sum(total_reward) / len(total_reward)
         total_reward = torch.tensor(total_reward).to(generated_images.device)
